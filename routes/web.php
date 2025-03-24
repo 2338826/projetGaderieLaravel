c32df998d771e33161bc834379f39ea85ff4d012
Thomas Chopin <2341922@cegeprdl.ca>
2025-03-24 14:30:57 -0400

    Routes and Controller

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [NurseryController::class, 'index']);
+Route::get('/garderie/{id}/edit', [NurseryController::class, 'edit'])->name('nursery.edit');
+Route::post('/garderies/add', [NurseryController::class, 'add'])->name('nursery.add');
+Route::put('/garderie/{id}/update', [NurseryController::class, 'update'])->name('nursery.update');
+Route::delete('/garderie/{id}/delete', [NurseryController::class, 'destroy'])->name('nursery.destroy');
+Route::delete('/garderie/clear', [NurseryController::class, 'clear'])->name('nursery.clear');
+Route::get('/Garderies', [NurseryController::class, 'show'])->name('nursery.show'); 
+route::get('/contact', [PostController::class, 'contact']);  
