b5276756dea573a9e5de15f59e7d1029937314af
Thomas Chopin <2341922@cegeprdl.ca>
2025-03-24 14:49:52 -0400

    Controller modify

--- app/Http/Controllers/NurseryController.php
+++ app/Http/Controllers/NurseryController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Nursery;
 use Illuminate\Http\Request;
 
 class NurseryController extends Controller
 {
     public function index()
     {
+        $nurseries = Nursery::orderBy('name')->get();
+
         return view('nursery');
     }
 }
